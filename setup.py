4051df9d283aeae227a70b07916b22fc468cbe6b
Bruno Rocha <rochacbruno@users.noreply.github.com>
2019-11-18 18:19:27 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flask_simple_sitemap',
+    name='flask_simplesitemap',
     version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
